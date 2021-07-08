6707763aa329e7dcc00a30c52fa7fde1de2b0442
Muukit <51193781+Muukit@users.noreply.github.com>
2021-07-09 03:33:44 +0600

    Update index.profile.jsx

--- src/components/profile/index.profile.jsx
+++ src/components/profile/index.profile.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Bio from './bio'
+import Skill from './skill'
+import Links from './link'
 
 class Profile extends React.Component {
   render (){
     return (
       <Bio/>
+      <Skill/>
+      <Link/>
     )
   }
 }
