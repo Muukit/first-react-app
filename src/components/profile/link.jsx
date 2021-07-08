bea8bcf2b7edaa6ea0077f7a120384dcdd866377
Muukit <51193781+Muukit@users.noreply.github.com>
2021-07-09 03:28:09 +0600

    Update link.jsx

---
+++ src/components/profile/link.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Links = () => {
+  <div className='Links'>
+      <ul>
+         <li><a href='#'> Facebook</a></li>
+         <li><a href='#'> Instagram</a></li>
+         <li><a href='#'> LinkedIn</a></li>
+         <li><a href='#'> Github</a></li>
+      </ul> 
+  </div>
+}
+
+export default Links;
