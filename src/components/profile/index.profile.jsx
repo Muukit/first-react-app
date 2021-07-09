ddd144689920bfd16e5e21528c49d4ca73cfc83d
Muukit <51193781+Muukit@users.noreply.github.com>
2021-07-09 17:57:46 +0600

    Update index.profile.jsx

--- src/components/profile/index.profile.jsx
+++ src/components/profile/index.profile.jsx
@@ -6,12 +6,15 @@ import Links from './link'
 class Profile extends React.Component {
   me = {
     name : 'Abdul Mukit Al Mehedy',
-    title : 'Junior web develope'
+    title : 'Junior web developer',
+    skillA : 'Javasctipt',
+     skillB : 'python' ,
+     skillC : 'java'
   }
   render (){
     return (
         <div className ='container'>
-            <Bio name={this.me.name } />
+            <Bio name={this.me.name } title={this.me.title}/>
        </div>
     )
   }
